7a2482cb03e4f5fdf7040e0d64d5ec3cb41edf20
David Goehrig <dave@dloh.org>
2013-12-01 15:38:29 -0500

    General cleanup
    Refactored a number of components to be a little cleaner

--- http.fs
+++ http.fs
@@ -12,7 +12,6 @@ create response size allot	\ create a big response buffer for http responses
 
 \ Configure the port we're listening on.
 8080 create-server value server
-." Server running on socket " server . cr
 
 \ The socket the client will use
 0 value client
@@ -48,35 +47,37 @@ create response size allot	\ create a big response buffer for http responses
 0 value referer:length
 0 value referer
 
+\ parse to end of line
+: >eol ( "input to newline" -- s-addr u )  10 parse ;
 
 \ Parsing methods TODO: implement POST HEAD OPTIONS PUT DELETE etc.
-: GET ." get method "  cr
+: GET 
 	parse-name to path:length to path
 	parse-name to version:length to version ;
 
-: User-Agent: ." got user agent " cr
-	10 parse to user-agent:length to user-agent ;
+: User-Agent:
+	>eol to user-agent:length to user-agent ;
 
-: Host: ." got host " cr
+: Host:
 	parse-name to host:length to host ;
 
-: Accept: ." got accept" cr
-	10 parse to accept:length to accept ;
+: Accept:
+	>eol to accept:length to accept ;
 
-: Accept-Language: ." got accept-language" cr
-	10 parse to accept-language:length to accept-language ;
+: Accept-Language:
+	>eol to accept-language:length to accept-language ;
 
-: Accept-Encoding: ." got accept-encoding" cr
-	10 parse to accept-encoding:length to accept-encoding ;
+: Accept-Encoding:
+	>eol to accept-encoding:length to accept-encoding ;
 
-: Connection: ." got connection" cr
+: Connection:
 	parse-name to connection:length to connection ;
 
-: Cache-Control: ." got cache-control" cr
-	10 parse to cache-control:length to cache-control ;
+: Cache-Control:
+	>eol to cache-control:length to cache-control ;
 
-: Referer: ." got referer" cr
-	10 parse to referer:length to referer ;
+: Referer:
+	>eol to referer:length to referer ;
 
 \ Routing code
 : /. 	\ this is the default path
@@ -88,35 +89,40 @@ create response size allot	\ create a big response buffer for http responses
 : http/404	\ this is an error page
 	s\" HTTP/1.1 404 Not Found\r\nContent-Length: 10\r\n\r\nNot Found\n" ;
 
-create command 4096 allot	\ path must be smaller than 4k because we only read 4k
+create route 4096 allot	\ path must be smaller than 4k because we only read 4k
+\ path to route
+: path>route 
+	s" /" path path:length compare 0= if  
+		2 route c! 
+		[char] / route 1+ c! 
+		[char] . route 2 + c! 
+	else
+		path:length route c! 
+		path route 1+ path:length cmove 
+	then ;
+
+
+\ tests if a route exists
+: route? ( -- true|false)
+	route find nip ;
+
+\ dispatch
+: dispatch route count evaluate ;
+
+: send-response
+	dup to response:length 		\ set the response length
+	response swap cmove		\ copy repond to response
+	response response:length client write-socket
+	client close-socket ;
 
 \ web server
 : serve begin
-	." serving " cr
 	server 255 listen
 	server accept-socket to client
-	." got client " client . cr
-	." accepted a socket " cr
-	client 0< invert if
-		client request size read-socket to request:length drop
-		request request:length type
-		request request:length evaluate
-		path path:length dup command c! command 1+ swap cmove
-		." command is " command count type cr
-		command find 0= if ." HTTP 404  not found " cr
-			drop http/404 
-		else drop
-			\ serve the index path if path is / otherwise route
-			s" /" path path:length compare 0= if  /. else
-				path path:length evaluate	( -- s-addr u )
-			then
-		then
-		dup to response:length 		\ set the response length
-		response swap cmove		\ copy repond to response
-		response response:length client write-socket
-		client close-socket
-	then
-again 
-;
+	client request size read-socket to request:length drop
+	request request:length evaluate
+	path>route route? if dispatch else http/404 then
+	send-response
+again ;
 
 serve
